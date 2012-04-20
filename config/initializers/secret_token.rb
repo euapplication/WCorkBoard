4637d0e6d3d65daea0541ddd547ba54e0c9c55ad
euapplication <sh_4221991@yahoo.co.jp>
2012-04-20 12:16:29 +0900

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WCorkBoard::Application.config.secret_token = '0df4c781c12363c3161169c4e24943155479317003cdb16175f6794a6361a4ad4c6003729de18e05979ffba10af224a10fca524296753201572bb28c7846c4c5'
